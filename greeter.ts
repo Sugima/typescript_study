47ed490a428428b3b492b9dae24967c241d1e0d9
Masahiro Sugiura <m.sugiura@comnico.jp>
2015-07-13 12:08:38 +0900

    class

---
+++ greeter.ts
@@ -0,0 +1,24 @@
+class Omikuji {
+  constructor() {
+
+    // ボタン要素
+    var buttonElement = document.getElementById("btn");
+
+    // 結果要素
+    var resultElement = document.getElementById("result");
+
+    // ボタンクリック時の処理
+    buttonElement.onclick = e => {
+      // 結果を格納
+      var results = ["大吉", "小吉", "中吉", "凶"];
+
+      // 乱数を生成
+      var result = Math.floor(Math.random() * 4);
+
+      // 乱数に基づいた結果を表示
+      resultElement.innerText = results[result];
+    };
+  }
+}
+
+var omikuji = new Omikuji();
